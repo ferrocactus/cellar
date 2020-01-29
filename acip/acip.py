fec269e000d73c68be8a590b6e59130d8493bd09
ferrocactus <euxhen_hasanaj@ymail.com>
2020-01-28 23:58:37 -0500

    lots of restructuring / decoupling

--- acip/acip.py
+++ acip/acip.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import json
 import tqdm
 import time
+import sys
 
 # Visualization and Dimensionality Reduction
 from umap import UMAP
@@ -20,236 +21,200 @@ from scipy import stats
 # Metrics
 from sklearn.metrics import mean_squared_error as mse, silhouette_score
 # Utils
-from utils.utils_visualization import reduce_and_plot
+from utils.utils_visualization import (plot_2d,
+                                    plot_explained_variance,
+                                    plot_gene_variances,
+                                    plot_scores,
+                                    plot_markers)
 from utils.utils_experiment import read_config
 # Constrained clustering
 from copkmeans.cop_kmeans import cop_kmeans
 from active_semi_clustering.semi_supervised.pairwise_constraints import PCKMeans
 
 class ACIP:
-    def __init__(self, x, y=None, config=None, verbose=False):
-        self.set_train_data(x, y)
+    def __init__(self, x, config="defaults", verbose=False, row_ids=None, col_ids=None):
+        self.set_train_data(x)
         # If use_config is set for a function, all its kwargs will be ignored
         self.config = config
+        self.read_config()
         self.verbose = verbose
+        self.set_ids(row_ids, col_ids)
     
-    def set_train_data(self, x, y=None, row_names=None, col_names=None):
+    def set_train_data(self, x):
         assert len(x.shape) == 2, "Data needs to be of shape (n x d)."
-        self.x_train = x
-        self.n_train = x.shape[0]
-        self.dims = x.shape[1]
-        self.y_train = y
-        self.row_names = row_names
-        self.col_names = col_names
+        self.x = x
+        self.rows = x.shape[0]
+        self.cols = x.shape[1]
     
-    def set_row_names(self, row_names):
-        self.row_names = row_names
+    def set_ids(self, row_ids=None, col_ids=None):
+        self.row_ids = row_ids # Cell IDs
+        self.col_ids = col_ids # Gene IDs
     
-    def set_col_names(self, col_names):
-        self.col_names = col_names
-
-    def set_config(self, config):
-        self.config = config
-
-    def set_verbose(self, verbose):
-        self.verbose = verbose
+    def read_config(self):
+        self.params = read_config(self.config)
     
     def normalize(self):
         raise NotImplementedError()
+
+    def print_verbose(self, step):
+        # Prints configs for given processing step. Reads from self.params
+        if self.verbose == False:
+            return
+        
+        if "method" in self.params[step]:
+            method = self.params[step]['method']
+            print("Using", method, "with params:")
+            pretty_print_dict(self.params[step][method])
+        else:
+            print("Using params:")
+            pretty_print_dict(self.params[step])
     
-    def boxplot(self):
+    def get_visual_emb(self):
         """
-        Boxplot of the columns of the data
+        Constructs 2d/3d embeddings for visualization.
+        If use_emb=1, then use embeddings x_emb otherwise use x.
+        Method should be one of UMAP, PCA, TSNE.
         """
-        if self.y_train is not None and self.verbose:
-            print("WARNING: This is a boxplot of the columns of the data. " +
-                    "Results may not be what expected.")
-        sns.set_style("whitegrid")
-        fig, ax = plt.subplots()
-        sns.boxplot(data=self.x_train)
+        use_x_emb = self.params['visual_emb']['use_x_emb']
+        if use_x_emb and not hasattr(self, 'x_emb'):
+            sys.exit("No embeddings found. Consider setting use_x_emb=0.")
+        print_header("Constructing visual embeddings.")
+        self.print_verbose('visual_emb')
 
-        sns.despine(left=True)
-        if self.col_names is not None and len(self.col_names) < 40:
-            plt.xticks(np.arange(self.dims), self.col_names, rotation=90)
-        plt.xlabel('Columns')
-        plt.ylabel('Rows')
-        plt.title('Boxplot')
-        fig.set_size_inches(10, 5)
-    
-    def reduce_plot(self, labels=None, method='umap', use_emb=True, **kwargs):
-        print_header("Visualizing")
-        if labels is None:      # By default use predicted labels
-            labels = self.y_train_pred
-        if self.config is not None:
-            kwargs = read_config(self.config)['reduce_plot'][method]
-            if self.verbose:
-                print('Using', method, 'with the following params:')
-                pretty_print_dict(kwargs)
-        self.x_train_2d_emb = reduce_and_plot(
-                        x=self.x_train_emb if use_emb else self.x_train,
-                        y=labels,
-                        method=method,
-                        **kwargs)
+        x = self.x_emb if use_x_emb else self.x
 
-    def pca_plot_var_ratio(self, n_components=None, **kwargs):
+        # Use the method specified in the config file
+        method = self.params['visual_emb']['method']
+        assert method in ["UMAP", "PCA", "TSNE"]
+        method_obj = globals()[method](**self.params['visual_emb'][method])
+        return method_obj.fit_transform(x)
+
+    def get_explained_variance(self):
         """
-        Plots the percentage of variance explained by each of the PCA components.
+        Returns the percentage of variance explained by each of the PCA components.
         """
-        kwargs['n_components'] = n_components or min(self.dims, self.n_train)
-        pca = PCA(**kwargs)
-        pca.fit(self.x_train)
+        print_header("Getting explained variance using PCA.")
+        self.print_verbose('explained_variance')
 
-        # Plotting
-        sns.set_style('whitegrid')
-
-        fig, (ax1, ax2) = plt.subplots(1, 2)
-        ax1.plot(list(range(1, kwargs['n_components'] + 1)),
-                pca.explained_variance_ratio_)
-        ax1.set_xlabel("Number of components")
-        ax1.set_ylabel("Percentage of Explained Variance per Component")
-        ax1.set_ylim(0)
-        # Cumulative plot
-        ax2.plot(list(range(1, kwargs['n_components'] + 1)),
-                np.cumsum(pca.explained_variance_ratio_))
-        ax2.set_xlabel("Number of components")
-        ax2.set_ylabel("Cumulative Percentage of Explained Variance")
-        ax2.set_ylim(0)
-
-        sns.despine()
-        fig.set_size_inches(10, 5)
+        pca = PCA(**self.params['explained_variance'])
+        pca.fit(self.x)
+        return list(range(1, pca.n_components_ + 1)), pca.explained_variance_ratio_
 
     def filter_genes(self):
         """
-        Computes the variance for every gene (column) and picks the highest 20%
-        variable genes. Stores the new matrix into self.x_train_filtered
+        Computes the variance for every gene (column). If 0 < n_genes <= 1, then
+        use the top (n_genes*cols) genes, otherwise use the top (n_genes). It is
+        possible to set n_genes="all" in which case no filtering is done.
         """
         print_header("Filtering genes")
-        gene_variances = np.var(self.x_train, axis=0)
-        highest_var_gene_indices = gene_variances.argsort()[-int(0.2 * self.x_train.shape[1]):]
-        self.x_train_filtered = self.x_train[:, highest_var_gene_indices]
+        self.print_verbose("filter_genes")
 
-        # Plotting
+        n_genes = self.params['filter_genes']['n_genes']
+        assert n_genes > 0
+
+        self.gene_variances = np.var(self.x, axis=0)
+        if n_genes <= 1: # Fraction of genes to keep
+            highest_var_gene_indices = self.gene_variances.argsort()[-int(n_genes * self.cols):]
+        else:            # Count of genes to keep
+            highest_var_gene_indices = self.gene_variances.argsort()[-n_genes:]
+        
         if self.verbose:
-            fig, ax = plt.subplots()
-            ax.hist(gene_variances)
-            ax.set_xlabel("Variance")
-            ax.set_ylabel("Gene Count")
-            sns.despine()
-            fig.set_size_inches(10, 5)
+            print("Keeping", len(highest_var_gene_indices), "genes.")
 
-    def reduce_dim(self, method='pca', **kwargs):
+        self.x_filtered = self.x[:, highest_var_gene_indices]
+
+    def reduce_dim(self):
         """
-        Reduces the dimensionality of the data and stores it in self.x_train_emb.
+        Reduces the dimensionality of the data and stores it in self.x_emb.
+        Requires filter_genes to be run first.
         """
+        if not hasattr(self, 'x_filtered'):
+            sys.exit("Genes not filtered. Run filter_genes first.")
         print_header("Reducing dimensionality")
-        print("Using " + method + ".")
-        if self.config is not None:  # Use the provided config file instead
-            kwargs = read_config(self.config)['reduce_dim'][method]
-            if self.verbose:
-                print('Using the following params:')
-                pretty_print_dict(kwargs)
+        self.print_verbose('reduce_dim')
 
-        if method == 'pca':
-            pca = PCA(**kwargs)
-            pca.fit(self.x_train_filtered)
-            # Print scores
-            self.x_train_emb = pca.transform(self.x_train_filtered)
+        method = self.params['reduce_dim']['method']
+        assert method in ["PCA"]
+        method_obj = globals()[method](**self.params['reduce_dim'][method])
+        
+        if method == 'PCA':
+            method_obj.fit(self.x_filtered)
+            self.x_emb = method_obj.transform(self.x_filtered)
             print("Embedding created. MSE:",
-                    mse(self.x_train_filtered, pca.inverse_transform(self.x_train_emb)))
-            print("Used", pca.n_components_, "components.")
-            #print("Train Average Log Likelihood:", pca.score(self.x_train_filtered))
+                    mse(self.x_filtered, method_obj.inverse_transform(self.x_emb)))
+            print("Used", method_obj.n_components_, "components.")
         else:
             raise NotImplementedError()
     
-    def cluster(self, method='kmedoids', n_clusters_list=list(range(2, 15, 2)), **kwargs):
+    def cluster(self):
         """
-        Clusters the embeddings as constructed by reduce_dim.
+        Clusters the embeddings created by reduce_dim. Tries various n_clusters
+        as specified by the list (low, high, step) and stores into y_pred the
+        highest scoring labels according to eval_method.
         """
+        if not hasattr(self, 'x_emb'):
+            sys.exit("Embeddings not created. Run reduce_dim first.")
         print_header("Clustering")
-        print("Using " + method + ".")
-        if self.config is not None:
-            kwargs = read_config(self.config)['cluster'][method]
-            if 'n_clusters' in kwargs:
-                n_clusters_list=[kwargs['n_clusters']]
-                kwargs.pop('n_clusters', None)
-            if self.verbose:
-                print("Using the following params:")
-                pretty_print_dict(kwargs)
+        self.print_verbose('cluster')
 
-        silhouette_score_list = []
-        max_sscore = -2
+        method = self.params['cluster']['method']
+        assert method in ["KMeans", "KMedoids", "SpectralClustering"]
+        eval_method = self.params['cluster']['eval_method']
+        assert eval_method in ["silhouette_score"]
+
+        # Construct list of n_clusters to try
+        low = self.params['cluster']['low']
+        high = self.params['cluster']['high']
+        step = self.params['cluster']['step']
+        self.n_clusters_list = list(range(low, high, step))
+        self.score_list = []
+        max_score = -np.Inf
         
         time.sleep(0.2) # To avoid tqdm bar from appearing before prints
-        pbar = tqdm.tqdm(n_clusters_list)
+        pbar = tqdm.tqdm(self.n_clusters_list)
         for n_clusters in pbar:
             pbar.set_description("Trying n_clusters=" + str(n_clusters))
-            if method == 'kmeans':
-                kmeans = KMeans(n_clusters=n_clusters, **kwargs)
-                y_train_pred = kmeans.fit_predict(self.x_train_emb)
-            elif method == 'spectral':
-                spectral = SpectralClustering(n_clusters=n_clusters, **kwargs)
-                y_train_pred = spectral.fit_predict(self.x_train_emb)
-            elif method == 'kmedoids':
-                kmedoids = KMedoids(n_clusters=n_clusters, **kwargs)
-                y_train_pred = kmedoids.fit_predict(self.x_train_emb)
-            else:
-                raise NotImplementedError()
-
-            sscore = silhouette_score(self.x_train_emb, y_train_pred, metric='correlation')
-            silhouette_score_list.append(sscore)
-            if max_sscore < sscore:
-                max_sscore = sscore
+            method_obj = globals()[method](n_clusters=n_clusters, **self.params['cluster'][method])
+            y_pred = method_obj.fit_predict(self.x_emb)
+            score = globals()[eval_method](self.x_emb, y_pred, **self.params['cluster'][eval_method])
+            self.score_list.append(score)
+            if max_score < score: # Update best score
+                max_score = score
                 self.n_clusters = n_clusters
-                self.y_train_pred = y_train_pred
+                self.y_pred = y_pred
         
-        print("Clustering complete.")
-        print("Highest silhouette score is achieved for n_clusters =", self.n_clusters)
-        
-        if self.verbose:
-            sns.set_style('whitegrid')
-            fig, ax = plt.subplots()
-            ax.plot(n_clusters_list, silhouette_score_list)
-            ax.set_xlabel('Number of Clusters')
-            ax.set_ylabel('Silhouette Score')
-            sns.despine()
-            fig.set_size_inches(10, 5)
+        print("\nClustering complete.")
+        print("Highest score is achieved for n_clusters =", self.n_clusters)
     
-    def get_markers(self):
+    def find_markers(self):
+        if not hasattr(self, 'y_pred'):
+            sys.exit("Clustering not performed. Run cluster first.")
+        print_header("Getting Markers")
+        self.print_verbose('markers')
+
+        alpha = self.params['markers']['alpha']
+        difference = self.params['markers']['difference']
+
         self.markers = []
+        self.pvals = np.zeros((self.n_clusters, self.cols))
+        self.mads = np.zeros((self.n_clusters, self.cols))
 
-        fig, ax = plt.subplots(self.n_clusters, 2)
-
+        time.sleep(0.2) # To avoid tqdm bar from appearing before prints
         for cluster_id in tqdm.tqdm(range(self.n_clusters), desc="Cluster id"):
-            x_in = self.x_train[np.where(self.y_train_pred == cluster_id)]
-            x_not_in = self.x_train[np.where(self.y_train_pred != cluster_id)]
+            x_in = self.x[np.where(self.y_pred == cluster_id)]
+            x_not_in = self.x[np.where(self.y_pred != cluster_id)]
 
-            pvals = []
-            mads = []
-            for gene in range(self.dims):
+            for gene in range(self.cols):
                 gene_vector_in = x_in[:, gene]
                 gene_vector_not_in = x_not_in[:, gene]
 
                 t, pval = stats.ttest_ind(gene_vector_in, gene_vector_not_in, equal_var=False)
-                pvals.append(pval)
-                mads.append(np.mean(gene_vector_in) - np.mean(gene_vector_not_in))
-            marker = self.col_names[np.where((np.array(pvals) < (0.05 / self.dims)) & (np.array(mads) > 1))]
+                self.pvals[cluster_id][gene] = pval
+                self.mads[cluster_id][gene] = np.mean(gene_vector_in) - np.mean(gene_vector_not_in)
+            marker = self.col_ids[np.where((np.array(self.pvals[cluster_id]) < (alpha / self.cols)) & (np.array(self.mads[cluster_id]) > difference))]
             self.markers.append(marker)
 
-            if self.verbose:
-                ax[cluster_id][0].hist(pvals)
-                ax[cluster_id][0].set_xlabel("p-values")
-                ax[cluster_id][0].set_ylabel("gene count")
-                ax[cluster_id][0].set_title("Cluster:" + str(cluster_id))
-                ax[cluster_id][1].hist(mads, color='r')
-                ax[cluster_id][1].set_xlabel("absolute difference")
-                ax[cluster_id][1].set_ylabel("gene count")
-                ax[cluster_id][1].set_title("Cluster:" + str(cluster_id))
-
         self.markers = np.asarray(self.markers)
-        
-        if self.verbose:
-            sns.despine()
-            fig.set_size_inches(10, self.n_clusters * 5)
     
     def cluster_constraints(self, method='agglomerative', n_clusters_list=list(range(2, 65, 2)), constraints=None, **kwargs):
         assert constraints is not None, "No constraints provided."
@@ -264,25 +229,25 @@ class ACIP:
         for n_clusters in pbar:
             pbar.set_description("Trying n_clusters=" + str(n_clusters))
             if method == 'cop_kmeans':
-                y_train_pred, centers = cop_kmeans(dataset=self.x_train_emb, k=n_clusters,
+                y_pred, centers = cop_kmeans(dataset=self.x_emb, k=n_clusters,
                                                     ml=constraints['ml'], cl=constraints['cl'],
                                                     **kwargs)
             elif method == 'agglomerative':
                 ac = AgglomerativeClustering(n_clusters=n_clusters, connectivity=constraints, **kwargs)
-                y_train_pred = ac.fit_predict(self.x_train_emb)
+                y_pred = ac.fit_predict(self.x_emb)
             elif method == 'pckmeans':
                 pck = PCKMeans(n_clusters=self.n_clusters)
-                pck.fit(self.x_train_emb, ml=constraints['ml'], cl=constraints['cl'])
-                y_train_pred = pck.labels_
+                pck.fit(self.x_emb, ml=constraints['ml'], cl=constraints['cl'])
+                y_pred = pck.labels_
             else:
                 raise NotImplementedError()
             
-            sscore = silhouette_score(self.x_train_emb, y_train_pred, metric='correlation')
+            sscore = silhouette_score(self.x_emb, y_pred, metric='correlation')
             silhouette_score_list.append(sscore)
             if max_sscore < sscore:
                 max_sscore = sscore
                 self.n_clusters = n_clusters
-                self.y_train_pred = y_train_pred
+                self.y_pred = y_pred
         
         print("Clustering with constraints complete.")
         print("Highest silhouette score is achieved for n_clusters =", self.n_clusters)
@@ -296,12 +261,30 @@ class ACIP:
             sns.despine()
             fig.set_size_inches(10, 5)
         
-    def flow(self, reduce_dim='pca', cluster='kmedoids', reduce_plot='umap'):
+    def flow(self, reduce_dim='pca', cluster='kmedoids', visualize='umap'):
         self.filter_genes()
-        self.reduce_dim(method=reduce_dim)
-        self.cluster(method=cluster)
-        self.reduce_plot(method=reduce_plot)
-        self.get_markers()
+        self.reduce_dim()
+        self.cluster()
+        self.find_markers()
+
+    def plot(self, what):
+        if what == "explained_variance":
+            plot_explained_variance(*self.get_explained_variance())
+        elif what == "gene_variances":
+            if not hasattr(self, 'gene_variances'):
+                sys.exit("No gene variances found. Run filter_genes.")
+            plot_gene_variances(self.gene_variances)
+        elif what == "cluster_scores":
+            if not hasattr(self, 'score_list'):
+                sys.exit("No scores found. Run cluster.")
+            plot_scores(self.n_clusters_list, self.score_list)
+        elif what == "markers":
+            if not hasattr(self, 'markers'):
+                sys.exit("No markers found. Run find_markers.")
+            plot_markers(self.n_clusters, self.pvals, self.mads)
+        elif what == "2d":
+            emb = self.get_visual_emb()
+            plot_2d(emb, self.y_pred)
 
 def pretty_print_dict(mydict):
     print(json.dumps(mydict, sort_keys=True, indent=4))
--- utils/utils_visualization.py
+++ utils/utils_visualization.py
@@ -2,6 +2,11 @@ from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import seaborn as sns
+from umap import UMAP
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+sns.set_style('whitegrid')
 
 def plot_images(imlist, cols=3, titlelist=None, cmap='gray'):
     """
@@ -17,48 +22,66 @@ def plot_images(imlist, cols=3, titlelist=None, cmap='gray'):
             plt.title(titlelist[i])
     plt.show()
 
-def reduce_and_plot(x=None, y=None, method='umap', dims=2, **kwargs):
-    """
-    Reduce the dimensionality of the data to 2D using the specified method
-    and plot.
-    params:
-        method: Method to use for dimensionality reduction;
-                choose between: umap, pca, t-sne
-        dims:   Plot dimension; Choose between 2 and 3
-    """
-    assert x is not None or emb is not None, "Data not provided."
+def plot_explained_variance(x, y, cumulative=True):
+    fig, ax = plt.subplots(1, cumulative+1, squeeze=False)
 
-    fig = plt.figure()
-    if   dims == 2:     ax = fig.add_subplot(111)
-    elif dims == 3:     ax = fig.add_subplot(111, projection='3d')
-    else:               raise NotImplementedError('Can only visualize 2 or 3 dims.')
+    ax[0][0].plot(x, y*100)
+    ax[0][0].set_xlabel("Number of components")
+    ax[0][0].set_ylabel("Percentage of Explained Variance per Component")
+    ax[0][0].set_ylim(0)
+    if cumulative:
+        ax[0][1].plot(x, np.cumsum(y*100))
+        ax[0][1].set_xlabel("Number of components")
+        ax[0][1].set_ylabel("Cumulative Percentage of Explained Variance")
+        ax[0][1].set_ylim(0)
 
-    print("Reducing dimensionality for visualization using " + method + ".")
-    if method == 'umap':
-        """
-        Possible arguments for UMAP and their default values
-            n_neighbors     = 15
-            min_dist        = 0.1
-            n_components    = 2
-            metric          = 'euclidean'
-        """
-        from umap import UMAP
-        emb = UMAP(n_components=dims, **kwargs).fit_transform(x)
-    elif method == 'pca':
-        from sklearn.decomposition import PCA
-        emb = PCA(n_components=dims, **kwargs).fit_transform(x)
-    elif method == 'tsne':
-        from sklearn.manifold import TSNE
-        emb = TSNE(n_components=dims, **kwargs).fit_transform(x)
-    else:
-        raise NotImplementedError('Method not found')
+    sns.despine()
+    fig.set_size_inches(5*(cumulative + 1), 5)
+
+def plot_gene_variances(gene_variances):
+    fig, ax = plt.subplots()
+    ax.hist(gene_variances)
+    ax.set_xlabel("Variance")
+    ax.set_ylabel("Gene Count")
+    sns.despine()
+    fig.set_size_inches(10, 5)
+
+def plot_scores(x, y):
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+    ax.set_xlabel('Number of Clusters')
+    ax.set_ylabel('Score')
+    sns.despine()
+    fig.set_size_inches(10, 5)
+
+def plot_markers(n_clusters, pvals, mads):
+    fig, ax = plt.subplots(n_clusters, 2)
+
+    for cluster_id in range(n_clusters):
+        ax[cluster_id][0].hist(pvals[cluster_id])
+        ax[cluster_id][0].set_xlabel("p-values")
+        ax[cluster_id][0].set_ylabel("gene count")
+        ax[cluster_id][0].set_title("Cluster:" + str(cluster_id))
+        ax[cluster_id][1].hist(mads[cluster_id], color='r')
+        ax[cluster_id][1].set_xlabel("absolute difference")
+        ax[cluster_id][1].set_ylabel("gene count")
+        ax[cluster_id][1].set_title("Cluster:" + str(cluster_id))
     
+    sns.despine()
+    fig.set_size_inches(10, self.n_clusters * 5)
+
+def plot_2d(x, y=None, dims=2):
+    fig = plt.figure()
+    if dims == 2: ax = fig.add_subplot(111)
+    elif dims == 3: ax = fig.add_subplot(111, projection='3d')
+    else: raise NotImplementedError('Can only visualize 2 or 3 dims.')
+
     if dims == 2:
         if y is not None:
             pal = []
             for i in range(len(np.unique(y))):
                 pal.append("#" + '%06x' % np.random.randint(16**6))
-            sns.scatterplot(x=emb[:, 0], y=emb[:, 1],
+            sns.scatterplot(x=x[:, 0], y=x[:, 1],
                             hue=y,
                             palette=pal,
                             linewidth=0,
@@ -66,15 +89,14 @@ def reduce_and_plot(x=None, y=None, method='umap', dims=2, **kwargs):
                             legend='full')
             plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
         else:
-            sns.scatterplot(x=emb[:, 0], y=emb[:, 1], linewidth=0, s=10)
+            sns.scatterplot(x=x[:, 0], y=x[:, 1], linewidth=0, s=10)
     else:
         if y is not None:
-            ax.scatter(emb[:, 0], emb[:, 1], emb[:, 2],
+            ax.scatter(x[:, 0], x[:, 1], x[:, 2],
                         c=[sns.color_palette()[i] for i in y])
         else:
-            ax.scatter(emb[:, 0], emb[:, 1], emb[:, 2])
+            ax.scatter(x[:, 0], x[:, 1], x[:, 2])
     sns.despine(left=True, bottom=True)
     plt.xticks([])
     plt.yticks([])
     fig.set_size_inches(10, 5)
-    return emb
