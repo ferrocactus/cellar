747470a5d7576c24a827296ea93e5ded60c04d0f
ferrocactus <euxhen_hasanaj@ymail.com>
2020-01-30 21:13:28 -0500

    updated example

--- example.py
+++ example.py
@@ -1,23 +1,14 @@
 from acip.acip import ACIP
-from utils.utils_experiment import load_data
+from utils.utils_experiment import read_config, load_data
 from matplotlib import pyplot as plt
+import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 
 if __name__ == '__main__':
-    plt.ion()
-
-    #plt.ioff()
     dataset = 'spleen'
     X, Y, gene_ids = load_data(dataset)
-    w = ACIP(X, Y, config=dataset, verbose=True)
-    w.set_col_names(gene_ids)
-    w.filter_genes()
-    w.reduce_dim(method='pca')
-    w.cluster(method='kmedoids')
-    w.reduce_plot(method='umap')
-    w.get_markers()
-    print(w.markers)
-
-    plt.ioff()
+    w = ACIP(X, config=dataset, verbose=False, col_ids=gene_ids)
+    w.flow()
+    w.plot("top_markers")
     plt.show()
