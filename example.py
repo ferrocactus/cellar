22f7829ff87c156af131d829941c6bb32e41b090
ferrocactus <euxhen_hasanaj@ymail.com>
2020-02-04 01:39:26 -0500

    fixed example

--- example.py
+++ example.py
@@ -7,9 +7,8 @@ warnings.filterwarnings('ignore')
 
 if __name__ == '__main__':
     dataset = 'spleen'
-    dataset = 'spleen'
     X, Y, gene_ids = load_data(dataset)
     w = ACIP(X, config=dataset, verbose=False, col_ids=gene_ids)
-    w.get_clusters_csv()
-    w.plot("top_markers")
+    w.flow()
+    w.plot("2d")
     plt.show()
