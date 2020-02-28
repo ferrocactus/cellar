c3dd0bf901b93a45f0a2d8eaff8f3cb398ab1bc8
ferrocactus <euxhen_hasanaj@ymail.com>
2020-02-28 02:36:36 -0500

    removed old datasets

--- src/utils/utils_experiment.py
+++ src/utils/utils_experiment.py
@@ -29,22 +29,9 @@ def load_data(dataset):
     if dataset == 'spleen':
         import anndata
         ann = anndata.read_h5ad('datasets/spleen/dim_reduced_clustered.h5ad')
-        return ann.X, ann.obs['leiden'].to_numpy().astype(np.int), ann.var.index.to_numpy().astype('U')
+        return ann.X, ann.var.index.to_numpy().astype('U')
     if dataset == 'spellman':
         import pandas as pd
         return pd.read_csv('datasets/Spellman.csv', index_col=0).to_numpy(), None
-    elif dataset == 'iris':
-        from sklearn.datasets import load_iris
-        X = load_iris()
-        return X.data, X.target
-    elif dataset == 'digits':
-        from sklearn.datasets import load_digits
-        X = load_digits()
-        return X.data, X.target, np.array([str(i) for i in range(1, 11)])
-    elif dataset == 'mnist':
-        from mnist import MNIST
-        mndata = MNIST('datasets/mnist')
-        images, labels = mndata.load_training()
-        return np.asarray(images), np.asarray(labels)
     else:
         raise FileNotFoundError('Dataset not supported.')
