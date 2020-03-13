e73601f6bd91469768b410e22da44ad2e52367bc
ferrocactus <euxhen_hasanaj@ymail.com>
2020-03-12 23:14:17 -0400

    fixed example

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from src import Pipeline
+from src import Pipeline, Plotter
 from src.utils.utils_experiment import load_data
 import warnings
 warnings.filterwarnings('ignore')
@@ -6,8 +6,7 @@ warnings.filterwarnings('ignore')
 if __name__ == '__main__':
     dataset = 'spleen'
     X, ids = load_data(dataset)
-    pipe = Pipeline(X, config='configs/'+dataset +
-                    ".ini", verbose=True, col_ids=ids)
+    pipe = Pipeline(X, config='configs/config.ini', verbose=True, col_ids=ids)
     pipe.run()
 
     plotter = Plotter(pipe)
