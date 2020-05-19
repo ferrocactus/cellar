922c1c180d31fa041e24c2df0f929a09596f97d2
ferrocactus <euxhen_hasanaj@ymail.com>
2020-05-19 09:20:15 -0400

    fixed scrolling

--- gui/ui.R
+++ gui/ui.R
@@ -27,57 +27,65 @@ sidebar <- dashboardSidebar(
 
 body <- dashboardBody(
     tags$head(includeCSS("./gui/custom.css")),
-        tabsetPanel(
-            type = "tabs",
-            id = "tabset",
-            tabPanel(
-                "Main Plot",
-                h3(textOutput("caption")),
-                plotlyOutput("plot"),
-            ),
-            tabPanel(
-                "Updated Plot",
-                verbatimTextOutput("brush"),
-                plotlyOutput("Plot2")
-            )
-            #tabPanel(
-             #   "Top Expressed Genes",
-              #   verbatimTextOutput("topgenes")
-            #),
+    # Don't scroll to top after clicking anything
+    tags$script(HTML(
+        "
+        $(document).on('click', function(e) {
+           e.preventDefault();
+        });
+        "
+    )),
+    tabsetPanel(
+        type = "tabs",
+        id = "tabset",
+        tabPanel(
+            "Main Plot",
+            h3(textOutput("caption")),
+            plotlyOutput("plot"),
         ),
-        tags$div(id = "placeholder"),
-        h3("Clusters and Intersections"),
-        tabsetPanel(id = "switcher",
-            tabPanel(
-                "No selection",
-                "No selection"
-            ),
-            tabPanel(
-                "DE",
-                verbatimTextOutput("genes"),
-                uiOutput("DEbuttons")
-            ),
-            tabPanel(
-                "Gene Ontology",
-                tableOutput("GeneOntology"),
-                downloadButton("downloadGO", "Download GO table")
-            ),
-            tabPanel(
-                "KEGG",
-                tableOutput("KEGG"),
-                downloadButton("downloadKEGG", "Download KEGG table")
-            ),
-            tabPanel(
-                "Markers Intersect",
-                tableOutput("Markers"),
-                downloadButton("downloadMKS", "Download Markers intersect table")
-            ),
-            tabPanel(
-                "MSigDB C2",
-                tableOutput("Msigdb"),
-                downloadButton("downloadMSIG", "Download MsigDB enrichment table")
-            )
+        tabPanel(
+            "Updated Plot",
+            verbatimTextOutput("brush"),
+            plotlyOutput("Plot2")
         )
+        #tabPanel(
+         #   "Top Expressed Genes",
+          #   verbatimTextOutput("topgenes")
+        #),
+    ),
+    tags$div(id = "placeholder"),
+    h3("Clusters and Intersections"),
+    tabsetPanel(id = "switcher",
+        tabPanel(
+            "No selection",
+            "No selection"
+        ),
+        tabPanel(
+            "DE",
+            verbatimTextOutput("genes"),
+            uiOutput("DEbuttons")
+        ),
+        tabPanel(
+            "Gene Ontology",
+            tableOutput("GeneOntology"),
+            downloadButton("downloadGO", "Download GO table")
+        ),
+        tabPanel(
+            "KEGG",
+            tableOutput("KEGG"),
+            downloadButton("downloadKEGG", "Download KEGG table")
+        ),
+        tabPanel(
+            "Markers Intersect",
+            tableOutput("Markers"),
+            downloadButton("downloadMKS", "Download Markers intersect table")
+        ),
+        tabPanel(
+            "MSigDB C2",
+            tableOutput("Msigdb"),
+            downloadButton("downloadMSIG", "Download MsigDB enrichment table")
+        )
+    )
 )
 
 ui <- dashboardPage(header, sidebar, body, skin='green')
