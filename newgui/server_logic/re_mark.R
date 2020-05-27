fb6a5b49f64f5b7e43d5e87954368455e20b4962
ferrocactus <euxhen_hasanaj@ymail.com>
2020-05-26 20:31:20 -0400

    fixed namespace problem when creating new btns

--- newgui/server_logic/re_mark.R
+++ newgui/server_logic/re_mark.R
@@ -1,5 +1,10 @@
 re_mark <- function(input, output, session, remark, pipe, deGenes, deButtons) {
     observe({ if (remark() > 0) {
+        if (length(deButtons()) > 0)
+            for (i in 1:length(deButtons()))
+                deButtons()[[i]]$destroy()
+        deButtons(c())
+
         output$genes <- renderTable({
             # TODO change '0' to whatever the first element is
             # not all clustering algorithms return cluster ids starting with 0
@@ -11,27 +16,24 @@ re_mark <- function(input, output, session, remark, pipe, deGenes, deButtons) {
                         stringsAsFactors = FALSE)
             table <- table[order(table[, 2], decreasing = TRUE),]
             colnames(table) <- c("DE genes", "logFC", "adj.pval")
-            deGenes(table[1:length(pipe()$markers[['0']][['outp_names']]), 1])
             return(table)
         })
 
-        if (length(deButtons()) > 0)
-            for (i in 1:length(deButtons()))
-                deButtons()[[i]]$destroy()
-        deButtons(c())
+        deGenes(pipe()$markers[['0']][['outp_names']])
+
+        ns <- session$ns
 
         output$DEbuttons <- renderUI({
             lapply(X = 1:length(deGenes()), FUN = function(i) {
-                    actionButton(paste(deGenes()[i], " ", seq = ""),
+                    actionButton(ns(paste(deGenes()[i], " ", seq = "")),
                                  paste(deGenes()[i], " ", seq = ""))})
         })
 
-
         lapply(X = 1:length(deGenes()), FUN = function(i) {
             o <- observeEvent(input[[paste(deGenes()[i], " ", seq = "")]], {
-                showNotification(
-                    paste("Showing ", deGenes()[i], "'s expression", sep = ""),
-                    duration=5)
+                #showNotification(
+                #    paste("Showing ", deGenes()[i], "'s expression", sep = ""),
+                #    duration=5)
             })
             deButtons(c(deButtons(), isolate(o)))
         })
