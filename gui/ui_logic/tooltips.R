73bb5a21e1ff2c18b442e92eb0d56e0d8854f5ca
Jingtao Wang <jingtao2@andrew.cmu.edu>
2020-06-25 23:50:03 -0400

    tooltips

--- gui/ui_logic/tooltips.R
+++ gui/ui_logic/tooltips.R
@@ -1,7 +1,23 @@
 tooltips <- function(id, label='tooltips') {ns = NS(id); list(
     # Dataset Menu
-
+    bsTooltip(ns("folder"),
+              paste0("Select the datasets on the server or the datasets uploaded by the user ",
+                     "type.")),
+    bsTooltip(ns("uploaded_dataset"),
+              paste0("Choose a specific dataset")),
+    bsTooltip(ns("file1"),
+              paste0("Upload a dataset")),
     # Clustering Menu
+    bsTooltip(ns("runconfigbtn"),
+              paste0("Do clustering with current configuration")),
+    bsTooltip(ns("dim_method"),
+              paste0("Choose a dimension reduction method",
+                     "type.")),
+    bsTooltip(ns("clu_method"),
+              paste0("Choose a clustering method")),
+    bsTooltip(ns("clu_method"),
+              paste0("Choose a clustering method",
+                     "type.")),
     bsTooltip(ns("dim_options"),
               paste0("If automatic, will try and detect the optimal ",
               "number of components using the ankle heuristic of finding the ",
@@ -34,22 +50,38 @@ tooltips <- function(id, label='tooltips') {ns = NS(id); list(
     # Selection & Labeling
     bsTooltip(ns("newsubset"),
               paste0("Store the selected points into a subset with this name.")),
+    bsTooltip(ns("newlabelbox"),
+              paste0("Enter the new label name you want to add")),
+    bsTooltip(ns("tissue"),
+              paste0("Select the tissue of the cell you want to label")),
+    bsTooltip(ns("newlabels"),
+              paste0("Select the cell type you want to label")),
     bsTooltip(ns("subset1_upd"),
               paste0("Update the name of this subset to the selected cell ",
                      "type.")),
 
     # Analysis
-    #bsTooltip(ns("subset1"),
-    #          paste0("Subset to run the analysis for against Subset 2.")),
-    #bsTooltip(ns("subset2"),
-    #          paste0("If None, will consider all cells not in Subset 1.")),
-    #bsTooltip(ns("searchgene"),
-    #          paste0("Gene name to search for.")),
+    bsTooltip(ns("subset1"),
+              paste0("Subset to run the analysis for against Subset 2.")),
+    bsTooltip(ns("subset2"),
+              paste0("If None, will consider all cells not in Subset 1.")),
+    bsTooltip(ns("searchgene"),
+             paste0("Gene name to search for.")),
 
+    
+    
     # Appearance
-    #bsTooltip(ns(""), paste0()),
+    
+    bsTooltip(ns("dot_size"), paste0("Select the size of the dots in the plot")),
+    bsTooltip(ns("show_names"), paste0("show cluster names in the plot")),
 
     # Export/Import Menu
+    bsTooltip(ns("download_sess"),
+              paste0("Download the current session for sharing or working later")),
+    bsTooltip(ns("upload_sess"),
+              paste0("Load a local session")),
+    bsTooltip(ns("plot_download_format"),
+              paste0("Select the format of the plot to be downloaded")),
     bsTooltip(ns("cell_subset_download"),
               paste0("Integer, list, or range specified by a dash, e.g., 1-7."))
 )}
