e1f286a95b4cfef993344b052d457e03c6fdc2a8
Jingtao Wang <jingtao2@andrew.cmu.edu>
2020-07-20 20:26:39 -0400

    uncertainty progress bar

--- gui/server_logic/uncertainty.R
+++ gui/server_logic/uncertainty.R
@@ -1,9 +1,15 @@
 
+
 uncertain <- function(input, output, session, adata) {
     observeEvent(input$uncertain,{
-        showNotification("calculating uncertainty")
-        cellar$get_neighbors(adata())
-        cellar$uncertainty(adata())
+        #showNotification("calculating uncertainty")
+        
+        withProgress(message = "Please Wait", value = 0, {
+            incProgress(1 / 3, detail = "Data processing...")
+            cellar$get_neighbors(adata())
+            incProgress(1 / 3, detail = "Calculating uncertainty...")
+            cellar$uncertainty(adata())
+        })
         showNotification("finished")
     })
 }
